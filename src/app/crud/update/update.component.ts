6ecafc02071f9c3067341c9876e4fa924435bb25
lperthel <lperthel@gmail.com>
2023-09-24 19:59:51 -0400

    Fixed indentation for update component

--- src/app/crud/update/update.component.ts
+++ src/app/crud/update/update.component.ts
@@ -7,20 +7,20 @@ import { Product } from '../product';
 @Component({
   selector: 'app-update',
   templateUrl: './update.component.html',
-  styleUrls: ['./update.component.scss']
+  styleUrls: ['./update.component.scss'],
 })
 export class UpdateComponent implements OnInit {
   productForm!: FormGroup;
   product!: Product;
-  initialProduct!: Product; // New property to hold the initial state of the product
-  formChanged = true; // New property to hold the state of whether the form has been changed
-  
+  initialProduct!: Product;
+  formChanged = true;
+
   constructor(
     private fb: FormBuilder,
     private crudService: CrudService,
     private route: ActivatedRoute,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.productForm = this.fb.group({
@@ -30,7 +30,7 @@ export class UpdateComponent implements OnInit {
       quantity: ['', [Validators.required]],
     });
 
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       const productId = params['productId'];
       if (productId) {
         this.crudService.getById(productId).subscribe((data: Product) => {
@@ -41,26 +41,32 @@ export class UpdateComponent implements OnInit {
       }
     });
   }
-
-  // New method to check if the form has been changed
   checkFormChanged() {
-    this.formChanged = JSON.stringify(this.initialProduct) !== JSON.stringify({ ...this.initialProduct, ...this.productForm.value });
+    this.formChanged =
+      JSON.stringify(this.initialProduct) !==
+      JSON.stringify({ ...this.initialProduct, ...this.productForm.value });
   }
 
   updateProduct() {
-    this.checkFormChanged(); // Check if the form has been changed when the user clicks "Update"
-    if (!this.formChanged) return; // If the form has not been changed, return early
-    
+    this.checkFormChanged();
+    if (!this.formChanged) return;
+
     if (this.productForm.valid && this.product && this.product.id) {
       const updatedProduct = { ...this.product, ...this.productForm.value };
-      this.crudService.update(this.product.id.toString(), updatedProduct).subscribe(() => {
-        console.log('Product updated', updatedProduct);
-        this.router.navigate(['/crud/home']);
-      });
+      this.crudService
+        .update(this.product.id.toString(), updatedProduct)
+        .subscribe(() => {
+          console.log('Product updated', updatedProduct);
+          this.router.navigate(['/crud/home']);
+        });
     }
   }
 
   cancel() {
     this.router.navigate(['/crud/home']);
+  }
+  isInvalid(controlName: string): boolean {
+    const control = this.productForm.get(controlName);
+    return !!control && control.invalid && control.touched;
   }
 }
